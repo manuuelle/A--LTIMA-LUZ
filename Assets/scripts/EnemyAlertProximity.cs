369d1df270c8ec3bcd6cd0f7883f1131003e78d9
manuuelle <n.manuelle@escolar.ifrn.edu.br>
2026-02-05 08:25:04 -0300

    atualizações

---
+++ Assets/scripts/EnemyAlertProximity.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyAudioProximity : MonoBehaviour
+{
+    public Transform player;          // Referência ao Player
+    public float detectionRadius = 5f; // Distância para tocar o som
+
+    private AudioSource audioSource;
+    private bool isPlaying = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        if (distance <= detectionRadius && !isPlaying)
+        {
+            audioSource.Play();
+            isPlaying = true;
+        }
+        else if (distance > detectionRadius && isPlaying)
+        {
+            audioSource.Stop();
+            isPlaying = false;
+        }
+    }
+}
+
